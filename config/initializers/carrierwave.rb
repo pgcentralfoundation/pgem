38e7029d0a4bf52c55e1a1c48a733016ab3d6532
Eugene Dubinin <eugend@commandprompt.com>
2025-02-12 15:45:20 +0200

    define asset_host specifically for carrierwave global one was messing with mailers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,5 @@ CarrierWave.configure do |config|
   config.storage = :file
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.enable_processing = false if Rails.env.test?
-  config.asset_host = ActionController::Base.asset_host
+  config.asset_host = "https://postgresconf.org"
 end
