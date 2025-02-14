ff1a2586b49024832de0819b4c3c2a937899415f
Eugene Dubinin <eugend@commandprompt.com>
2025-02-14 13:11:41 +0200

    Revert "define asset_host specifically for carrierwave"
    This reverts commit 38e7029d0a4bf52c55e1a1c48a733016ab3d6532.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,5 @@ CarrierWave.configure do |config|
   config.storage = :file
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.enable_processing = false if Rails.env.test?
-  config.asset_host = "https://postgresconf.org"
+  config.asset_host = ActionController::Base.asset_host
 end
