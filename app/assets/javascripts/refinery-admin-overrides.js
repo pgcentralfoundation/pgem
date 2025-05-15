b54d1245fad51ad6ffd1f80cad2f682f5e3df338
Eugene Dubinin <eugend@commandprompt.com>
2025-05-15 15:41:12 +0300

    remove refinery js stuff

--- app/assets/javascripts/refinery-admin-overrides.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $(document).ready(function(){
-      $('#actions').find('li:has(a.add_icon)').addClass('cta')
-      $('#actions').find('li:has(a.page_add_icon)').addClass('cta')
-      $('#actions').find('li:has(a.upload_icon)').addClass('cta')
-    });
-});
