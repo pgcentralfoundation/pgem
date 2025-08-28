5575daa8528646c99f74bed37067d922ad0f09bf
Eugene Dubinin <eugend@commandprompt.com>
2025-08-28 12:37:17 +0300

    rewrite EventUser.set_is_highlight without using low-level deprecated APIs

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -6,6 +6,6 @@ class EventUser < ActiveRecord::Base
   belongs_to :user
 
   def self.set_is_highlight(program, user, is_highlight)
-    ActiveRecord::Base.connection.execute("UPDATE event_users SET is_highlight = #{ActiveRecord::Base.sanitize(is_highlight)} FROM events WHERE event_users.event_id = events.id AND events.program_id = #{ActiveRecord::Base.sanitize(program.id)} AND event_users.user_id = #{ActiveRecord::Base.sanitize(user.id)}")
+    program.event_users.where(user: user).update(is_highlight: is_highlight)
   end
 end
