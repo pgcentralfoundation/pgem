261ec0cf3965300f4341c60ffa45b9f1ff1de361
Eugene Dubinin <eugend@commandprompt.com>
2026-09-03 17:48:29 +0300

    fix conference/about page crash

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,8 @@
 class AboutController < ApplicationController
+  # thumbnail_url (used in the team member section) is defined in this
+  # Spina helper, normally only auto-included in Spina-namespaced views.
+  helper Spina::ImagesHelper
+
   load_and_authorize_resource :conference, find_by: :short_title
     
   def show
