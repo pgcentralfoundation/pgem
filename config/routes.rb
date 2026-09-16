107e78ed5ce0e2fa1918c6415b6b645b9c96cbea
Eugene Dubinin <eugend@commandprompt.com>
2026-09-16 16:09:31 +0300

    disable redundant/duplicate routes for passwordless signin flow

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Osem::Application.routes.draw do
                path: 'accounts'
     namespace "passwordless" do
       devise_for :users,
+        skip: [:passwords, :registrations, :confirmations, :magic_link],
         controllers: { sessions: "users/passwordless_sessions" }
     end
   end
